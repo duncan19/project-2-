5e0a9632a1382b6a38fc380a477f1d8e1b37ab6d
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-17 15:03:43 -0800

    add results varrying

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,13 @@ $("#code-Form").submit(function(){
     var sport = parseInt($("#sport").val());
     var shoe =  parseInt($("#shoe-size").val())
 
-
+    if (color + time + pet + sport + shoe === 10) {
+          $("#language-c#").show();
+        } else if (color + time + pet + sport + shoe  > 10){
+          $("#naguage-ruby").show();
+        } else {
+          $("#language-python").show();
+        }
 
 
 
