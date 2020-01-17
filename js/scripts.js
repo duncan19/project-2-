16b9c9c6de20430e8f89b16ae771ea004812963b
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-17 14:16:16 -0800

    start adding js to the webpage

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function{
+$("#code-Form").submit(function(){
+    var color =  parseInt($("#color").val());
+    var time =  parseInt($("#time").val());
+    var pet =  parseInt($("#pet").val());
+    var sport = parseInt($("#sport").val());
+    var shoe =  parseInt($("#shoe-size").val())
+
+
+
+
+
+
+})
+})
