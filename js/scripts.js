663a79e56998c7f3f153c5e87fdd093295b6ed04
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-17 16:55:33 -0800

    fix spacing errors within the code and clear out logs and commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,25 @@
 $(document).ready(function() {
-$("#code-Form").submit(function(){
-    var color =  parseInt($("#color").val());
-    var time =  parseInt($("#time").val());
-    var pet =  parseInt($("#pet").val());
-    var sport = parseInt($("#sport").val());
-    var shoe =  parseInt($("#shoe-size").val())
+  $("#code-Form").submit(function(){
+      var color =  parseInt($("#color").val());
+      var time =  parseInt($("#time").val());
+      var pet =  parseInt($("#pet").val());
+      var sport = parseInt($("#sport").val());
+      var shoe =  parseInt($("#shoe-size").val())
 
-    $(".main").hide();
-    console.log("test");
-    $(".results").show();
-    if (color + time + pet + sport + shoe === 10) {
-          $(".language-c").show();
-        } else if (color + time + pet + sport + shoe  > 10){
-          $(".language-ruby").show();
-        } else {
-          $(".language-python").show();
-        }
+      $(".main").hide();
+      $(".results").show();
+      if (color + time + pet + sport + shoe === 10) {
+            $(".language-c").show();
+          } else if (color + time + pet + sport + shoe  > 10){
+            $(".language-ruby").show();
+          } else {
+            $(".language-python").show();
+          }
 
-    event.preventDefault();
-$("#reset-btn").click(function(){
-  location.reload();
-  })
+      event.preventDefault();
+    $("#reset-btn").click(function(){
+      location.reload();
+    });
 
-})
-})
+  });
+});
