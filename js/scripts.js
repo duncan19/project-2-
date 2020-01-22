1fab7ffbe7ab0a90c1881a10829f1ffdd73e2245
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-21 23:08:02 -0800

    fix spacing in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
 $(document).ready(function() {
   $("#code-Form").submit(function(){
-      var color =  parseInt($("#color").val());
-      var time =  parseInt($("#time").val());
-      var pet =  parseInt($("#pet").val());
-      var sport = parseInt($("#sport").val());
-      var shoe =  parseInt($("#shoe-size").val())
+    var color =  parseInt($("#color").val());
+    var time =  parseInt($("#time").val());
+    var pet =  parseInt($("#pet").val());
+    var sport = parseInt($("#sport").val());
+    var shoe =  parseInt($("#shoe-size").val())
 
-      $(".main").hide();
-      $(".results").show();
-      if (color + time + pet + sport + shoe === 10) {
-            $(".language-c").show();
-          } else if (color + time + pet + sport + shoe  > 10){
-            $(".language-ruby").show();
-          } else {
-            $(".language-python").show();
-          }
-
-      event.preventDefault();
+    $(".main").hide();
+    $(".results").show();
+    if (color + time + pet + sport + shoe === 10) {
+      $(".language-c").show();
+    } else if (color + time + pet + sport + shoe  > 10){
+      $(".language-ruby").show();
+    } else {
+      $(".language-python").show();
+    }
+    event.preventDefault();
     $("#reset-btn").click(function(){
       location.reload();
     });
-
   });
 });
